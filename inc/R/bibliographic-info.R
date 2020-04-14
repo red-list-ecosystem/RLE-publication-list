f0d85bd17aa50b6e5763fdf97c5fc79f1015d6cd
jrfep <jr.ferrer.paris@gmail.com>
2020-04-14 10:22:00 +1000

    description of web pages

--- inc/R/bibliographic-info.R
+++ inc/R/bibliographic-info.R
@@ -1,7 +1,23 @@
 #!R --vanilla
 require(bibliometrix)
+require(readODS)
 
 A0 <- readFiles("~/proyectos/IUCN/RLE-publication-list/bibtex/WOS-search-20200413.bib")
 M0 <- convert2df(A0, dbsource = "isi", format = "bibtex")
 
 mis.dois <- read.csv("~/proyectos/IUCN/RLE-publication-list/input/DOI-check-list.txt",header=F,as.is=T)$V1
+load("~/proyectos/IUCN/RLE-publication-list/Rdata/CR-biblio-info.rda")
+
+a.step <- read.csv("~/proyectos/IUCN/RLE-publication-list/input/assessment-step.csv",header=F,as.is=T)
+
+a.units <- read_ods("~/proyectos/IUCN/RLE-publication-list/input/List_assessment_units.ods",sheet=1)
+
+sort(table(a.units$ReferenceID))
+
+a.sumr <- read_ods("~/proyectos/IUCN/RLE-publication-list/input/Summary_RLE_database.ods",sheet=1)
+
+a.dois <- grep("^10",unique(a.sumr$DOI),value=T)
+
+
+ subset(a.step,!V1 %in% mis.dois)$V1
+ subset(a.dois,!a.dois %in% mis.dois)
