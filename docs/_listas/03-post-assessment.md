30c59c22c91a558b54dee4ae6f8807360ee75a11
jrfep <jr.ferrer.paris@gmail.com>
2020-05-06 08:41:37 +1000

    updating assessment lists

--- docs/_listas/03-post-assessment.md
+++ docs/_listas/03-post-assessment.md
@@ -2,4 +2,10 @@
 title: Post-assessment publications
 ---
 
-Publications that summarize prior assessment outcomes.
+Publications that summarize assessment outcomes.
+
+*  (2014-07) [Separating risks from values in setting priorities for plant community conservation](http://doi.org/10.1111/avsc.12112)
+ *  (2015-06) [Assessing and managing risks to ecosystem biodiversity](http://doi.org/10.1111/aec.12249)
+ *  (2015-07-03) [Harnessing biodiversity and conservation knowledge products to track the Aichi Targets and Sustainable Development Goals](http://doi.org/10.1080/14888386.2015.1075903)
+ *  (NA) [Assessing the Cost of Global Biodiversity and Conservation Knowledge](http://doi.org/10.1371/journal.pone.0160640)
+ *  (2018-06) [Habitat conservation in Italy: the state of the art in the light of the first European Red List of Terrestrial and Freshwater Habitats](http://doi.org/10.1007/s12210-018-0688-5)
--- inc/R/bibliographic-info.R
+++ inc/R/bibliographic-info.R
@@ -1,6 +1,7 @@
 #!R --vanilla
 require(bibliometrix)
 require(readODS)
+require(dplyr)
 
 A0 <- readFiles("~/proyectos/IUCN/RLE-publication-list/bibtex/WOS-search-20200413.bib")
 M0 <- convert2df(A0, dbsource = "isi", format = "bibtex")
@@ -22,9 +23,51 @@ a.dois <- grep("^10",unique(a.sumr$DOI),value=T)
  subset(a.step,!V1 %in% mis.dois)$V1
  subset(a.dois,!a.dois %in% mis.dois)
 
-
-output.arch <- "~/proyectos/IUCN/RLE-publication-list/docs/_posts/pre-assessment.md"
+#### pre assessment
 slc <- subset(a.step,V2 %in% "pre assessment")$V1
-require(dplyr)
+
+output.arch <- "~/proyectos/IUCN/RLE-publication-list/docs/_listas/01-pre-assessment.md"
+
+cat("---
+title: Pre-assessment publications
+---
+References with information related to guidelines, methods and data gathering prior to the proper assessment.
+
+",file=output.arch)
+
 ref.info$data %>% filter(doi %in% slc) %>% arrange(created) -> dts
-cat(file=output.arch,with(dts,sprintf("* %s (%s) [%s](http://doi.org/%s)\n",paste(author$family,collapse="; "), published.print,title,doi)))
+cat(file=output.arch,with(dts,sprintf("* %s (%s) [%s](http://doi.org/%s)\n",paste(author$family,collapse="; "), published.print,title,doi)),append=T)
+
+
+#### assessment
+slc <- subset(a.step,V2 %in% "assessment")$V1
+
+output.arch <- "~/proyectos/IUCN/RLE-publication-list/docs/_listas/02-assessment.md"
+
+cat("---
+title: Assessment publications
+---
+
+Actual implementation of the assessment following one set of guidelines, and presenting a proper outcome for each assessment unit
+
+",file=output.arch)
+
+ref.info$data %>% filter(doi %in% slc) %>% arrange(created) -> dts
+cat(file=output.arch,with(dts,sprintf("* %s (%s) [%s](http://doi.org/%s)\n",paste(author$family,collapse="; "), published.print,title,doi)),append=T)
+
+
+#### post-assessment
+slc <- subset(a.step,V2 %in% "post assessment")$V1
+
+output.arch <- "~/proyectos/IUCN/RLE-publication-list/docs/_listas/03-post-assessment.md"
+
+cat("---
+title: Post-assessment publications
+---
+
+Publications that summarize assessment outcomes.
+
+",file=output.arch)
+
+ref.info$data %>% filter(doi %in% slc) %>% arrange(created) -> dts
+cat(file=output.arch,with(dts,sprintf("* %s (%s) [%s](http://doi.org/%s)\n",paste(author$family,collapse="; "), published.print,title,doi)),append=T)
